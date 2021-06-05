ea360a2456b8baa6f45760277f4ba096900ffd89
audiomuse1 <andrewhartford@utexas.edu>
2021-06-05 14:41:45 -0500

    mistake

--- README.md
+++ README.md
@@ -8,4 +8,7 @@ Run `npm run deploy`.
 
 Portfolio is currently deployed at: https://audiomuse1.github.io/portfolio/
 
+
+Add a custom scrollbar: https://www.freecodecamp.org/news/css-scrollbar-tutorial/
+
 Usage of animations: https://digital-flowers.github.io/react-animated-css.html
--- src/components/Navitem.js
+++ src/components/Navitem.js
@@ -4,7 +4,7 @@ class Navitem extends Component {
 render() {
 return (
 <li id={this.props.item}>
-<Link to={this.props.tolink} onClick={this.props.activec.bind(this,this.props.item)}>{this.props.item}</Link>
+<Link to={this.props.tolink} onClick={this.props.c.bind(this,this.props.item)}>{this.props.item}</Link>
 </li>
 )
 }
